6aec16adce01cc8b07f91946866dc33e45e817f0
Arturo Gutierrez <agutierrez.gyo@gmail.com>
2026-07-02 14:50:08 -0300

    Feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 
@@ -22,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={manrope.variable}>{children}</body>
+      <body className={manrope.variable}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
